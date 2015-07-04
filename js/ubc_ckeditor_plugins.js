140fa3ac8d8eda1679f6040e9c5471584c0f01c0
James <james.otoole@ubc.ca>
2015-07-03 17:07:06 -0700

    Fix incorrect js selector

--- js/ubc_ckeditor_plugins.js
+++ js/ubc_ckeditor_plugins.js
@@ -1,7 +1,7 @@
 (function ($) {
  	Drupal.behaviors.ubc_ckeditor_plugins = {
 		attach: function (context) {
-			$('.accordion-formatted', context).once('ubc_ckeditor_plugins', function () {
+			$('.accordion', context).once('ubc_ckeditor_plugins', function () {
 				$(this).find('.accordion-tab').click(function(e){
 					e.preventDefault();
 					//Expand or collapse this panel
